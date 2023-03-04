56f6e3c7f6cc89be90ab4d63103cbcfe9e21272a
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-04 19:34:43 +0200

    add data packegs and class{

---
+++ app/src/main/java/com/example/giveandtake/data/MyRoomDataBase.java
@@ -0,0 +1,9 @@
+package com.example.giveandtake.data;
+
+import android.media.Image;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+public abstract class MyRoomDataBase extends RoomDatabase {
+}
