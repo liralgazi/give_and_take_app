2fef445fa1042b230688488c24e4fff91ec7c6fd
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-05 23:14:26 +0200

    add code from class

---
+++ app/src/main/java/com/example/giveandtake/StudentsListFragmentViewModel.java
@@ -0,0 +1,17 @@
+//package com.example.giveandtake;
+//
+//import androidx.lifecycle.LiveData;
+//import androidx.lifecycle.ViewModel;
+//
+//import com.example.class3demo2.model.Model;
+//import com.example.class3demo2.model.Student;
+//
+//import java.util.List;
+//
+//public class StudentsListFragmentViewModel extends ViewModel {
+//    private LiveData<List<Student>> data = Model.instance().getAllStudents();
+//
+//    LiveData<List<Student>> getData(){
+//        return data;
+//    }
+//}
