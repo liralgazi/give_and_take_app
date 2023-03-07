e9ce0cdc6bad8168fd97d4f49b239edbb901a219
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-07 23:39:17 +0200

    add upload user function

--- app/src/main/java/com/example/giveandtake/fragments/CreateAccountFragment.java
+++ app/src/main/java/com/example/giveandtake/fragments/CreateAccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.giveandtake.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,11 +16,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.giveandtake.FragmentReplacerActivity;
+import com.example.giveandtake.MainActivity;
 import com.example.giveandtake.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateAccountFragment extends Fragment {
 
@@ -102,12 +109,36 @@ public class CreateAccountFragment extends Fragment {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-
+                    FirebaseUser user = auth.getCurrentUser();
+                    uploadUser(user, name, email);
                 }else{
                     String exception = task.getException().getMessage();
                     Toast.makeText(getContext(), "Error: "+ exception, Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void uploadUser(FirebaseUser user, String name, String email) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("name", name);
+        map.put("email",  email);
+        map.put("profileImage", "");
+        map.put("uid", user.getUid());
+
+        FirebaseFirestore.getInstance().collection("Users").document(user.getUid()).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    assert getContext() != null;
+                    startActivity(new Intent(getContext().getApplicationContext(), MainActivity.class));
+                }else{
+                    String exception = task.getException().getMessage();
+                    Toast.makeText(getContext(), "Error: "+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
