cedf72fdab82014d26ae7603988d2de9ebf01077
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-07 22:45:08 +0200

    create new login fragment and change style

---
+++ app/src/main/java/com/example/giveandtake/fragments/loginFragment.java
@@ -0,0 +1,27 @@
+package com.example.giveandtake.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.giveandtake.R;
+
+public class loginFragment extends Fragment {
+
+
+    public loginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
