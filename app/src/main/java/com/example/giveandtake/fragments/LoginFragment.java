00e39662074feaa22a72f8a8b1a11fbbfe9e3a4b
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-08 09:09:46 +0200

    add login progress bar

--- app/src/main/java/com/example/giveandtake/fragments/LoginFragment.java
+++ app/src/main/java/com/example/giveandtake/fragments/LoginFragment.java
@@ -7,11 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.giveandtake.R;
 
 public class LoginFragment extends Fragment {
 
+    private EditText emailEt, passwordEt;
+    private TextView signUpTv;
+    private Button loginBtn, SignUpBtn;
+    private ProgressBar progressBar;
 
     public LoginFragment() {
         // Required empty public constructor
