d1c652b6e66bf74b25c780434db17eef55705557
liralgazi <liralgazi@gmail.com>
2023-03-10 21:55:57 +0200

    next and back image btns and bug fix

--- app/src/main/java/com/example/giveandtake/fragments/Add.java
+++ app/src/main/java/com/example/giveandtake/fragments/Add.java
@@ -2,13 +2,17 @@ package com.example.giveandtake.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.giveandtake.R;
@@ -19,6 +23,7 @@ public class Add extends Fragment {
     private ImageView imageView;
     private RecyclerView recyclerView;
 
+    private ImageButton backBtn, nextBtn;
     public Add() {
         // Required empty public constructor
     }
@@ -28,5 +33,22 @@ public class Add extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+        recyclerView.setHasFixedSize(true);
+    }
+
+    private void init(View view)
+    {
+        descET = view.findViewById(R.id.descriptionET);
+        imageView = view.findViewById(R.id.imageView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        backBtn = view.findViewById(R.id.backBtn);
+        nextBtn = view.findViewById(R.id.nextBtn);
     }
 }
