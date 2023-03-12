4e2a0cdae2fc2d1c19c06a54176f44251f92a838
liralgazi <liralgazi@gmail.com>
2023-03-12 23:08:32 +0200

    search

--- app/src/main/java/com/example/giveandtake/adapter/UserAdapter.java
+++ app/src/main/java/com/example/giveandtake/adapter/UserAdapter.java
@@ -3,20 +3,28 @@ package com.example.giveandtake.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.giveandtake.R;
+import com.example.giveandtake.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
 
 
-    private List<Users> list;
+    private List<User> list;
 
-    public UserAdapter(List<Users> list) {
+    public UserAdapter(List<User> list) {
         this.list = list;
     }
 
@@ -30,6 +38,20 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
     @Override
     public void onBindViewHolder(@NonNull UserHolder holder, int position) {
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (list.get(position).getId().equals(user.getUid()))
+        {
+            holder.layout.setVisibility(View.GONE);
+        }
+
+        holder.nameTV.setText(list.get(position).getName());
+        holder.statusTV.setText(list.get(position).getVolunteerStatus());
+        Glide.with(holder.itemView.getContext().getApplicationContext())
+                .load(list.get(position).getProfileImageURL())
+                .placeholder(R.drawable.ic_person)
+                .timeout(6500)
+                .into(holder.profileImage);
+
     }
 
     @Override
@@ -39,9 +61,18 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
 
     class UserHolder extends RecyclerView.ViewHolder{
 
+        private CircleImageView profileImage;
+        private TextView nameTV, statusTV;
+        private RelativeLayout layout;
 
         public UserHolder(@NonNull View itemView) {
             super(itemView);
+            profileImage = itemView.findViewById(R.id.profileImage);
+            nameTV = itemView.findViewById(R.id.nameTv);
+            statusTV = itemView.findViewById(R.id.statusTv);
+            layout = itemView.findViewById(R.id.relativeLayout);
+
+
         }
     }
 
--- app/src/main/java/com/example/giveandtake/fragments/Search.java
+++ app/src/main/java/com/example/giveandtake/fragments/Search.java
@@ -15,7 +15,11 @@ import android.widget.SearchView;
 
 import com.example.giveandtake.R;
 import com.example.giveandtake.adapter.UserAdapter;
+import com.example.giveandtake.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
@@ -31,7 +35,8 @@ public class Search extends Fragment {
     SearchView searchView;
     RecyclerView recyclerView;
     UserAdapter adapter;
-    private List<Users>  list;
+    private List<User> list;
+    CollectionReference reference;
 
     public Search() {
         // Required empty public constructor
@@ -49,9 +54,35 @@ public class Search extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         init(view);
-
+        reference = FirebaseFirestore.getInstance().collection("Users");
         loadUserDate();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchUser();
+    }
+
+    private void loadUserDate()
+    {
+        reference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null)
+                    return;
+
+                if (value == null)
+                    return;
+                list.clear();
+                for (QueryDocumentSnapshot snapshot : value)
+                {
+                    User user = snapshot.toObject(User.class);
+                    list.add(user);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+
+    /*
+     searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
 
@@ -64,30 +95,42 @@ public class Search extends Fragment {
                 return false;
             }
         });
-    }
-
-    private void loadUserDate()
+     */
+    private void searchUser()
     {
-        CollectionReference reference = FirebaseFirestore.getInstance().collection("Users");
-        reference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error != null)
-                    return;
+            public boolean onQueryTextSubmit(String query) {
 
-                if (value == null)
-                    return;
-                list.clear();
-                for (QueryDocumentSnapshot snapshot : value)
-                {
-                    Users users = snapshot.toObject(Users.class);
-                    list.add(users);
-                }
-                adapter.notifyDataSetChanged();
+                reference.orderBy("name").startAt(query).endAt(query+"\uf8ff")
+                        .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if (task.isSuccessful())
+                                {
+                                    for (DocumentSnapshot snapshot: task.getResult())
+                                    {
+                                        if(!snapshot.exists())
+                                            return;
+
+                                        User user = snapshot.toObject(User.class);
+                                        list.add(user);
+                                    }
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        });
+                return false;
+
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
     }
-
     private void init(View view)
     {
         searchView = view.findViewById(R.id.searchView);
@@ -95,7 +138,7 @@ public class Search extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        list = new ArrayList<>();
+        list = new ArrayList<User>();
         adapter = new UserAdapter(list);
         recyclerView.setAdapter(adapter);
 
