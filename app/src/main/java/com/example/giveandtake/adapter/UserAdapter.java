26a4fb14b2708d87d2e58f435e68a62783a5ea5e
liralgazi <liralgazi@gmail.com>
2023-03-12 22:05:26 +0200

    users and useritem.xml files

---
+++ app/src/main/java/com/example/giveandtake/adapter/UserAdapter.java
@@ -0,0 +1,49 @@
+package com.example.giveandtake.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.giveandtake.R;
+import com.example.giveandtake.model.Users;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserHolder> {
+
+
+    private List<Users> list;
+
+    public UserAdapter(List<Users> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public UserHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_items, parent, false);
+        return new UserHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class UserHolder extends RecyclerView.ViewHolder{
+
+
+        public UserHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
--- app/src/main/java/com/example/giveandtake/fragments/Search.java
+++ app/src/main/java/com/example/giveandtake/fragments/Search.java
@@ -2,15 +2,39 @@ package com.example.giveandtake.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.giveandtake.R;
+import com.example.giveandtake.adapter.UserAdapter;
+import com.example.giveandtake.model.Users;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Search extends Fragment {
+
+
+    SearchView searchView;
+    RecyclerView recyclerView;
+    UserAdapter adapter;
+    private List<Users>  list;
 
     public Search() {
         // Required empty public constructor
@@ -21,5 +45,62 @@ public class Search extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+
+        loadUserDate();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+    }
+
+    private void loadUserDate()
+    {
+        CollectionReference reference = FirebaseFirestore.getInstance().collection("Users");
+        reference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null)
+                    return;
+
+                if (value == null)
+                    return;
+                list.clear();
+                for (QueryDocumentSnapshot snapshot : value)
+                {
+                    Users users = snapshot.toObject(Users.class);
+                    list.add(users);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void init(View view)
+    {
+        searchView = view.findViewById(R.id.searchView);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        list = new ArrayList<>();
+        adapter = new UserAdapter(list);
+        recyclerView.setAdapter(adapter);
+
     }
 }
