651c4b37d1805e535558050cffaafc3a5018bff3
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-03-19 17:21:47 +0200

    add navigate

---
+++ app/src/main/java/com/example/giveandtake/fragments/profile/Profile.java
@@ -0,0 +1,150 @@
+package com.example.giveandtake.fragments.profile;
+
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.giveandtake.MainActivity;
+import com.example.giveandtake.R;
+import com.example.giveandtake.databinding.FragmentProfileBinding;
+import com.example.giveandtake.fragments.home.HomeListFragmentViewModel;
+import com.example.giveandtake.fragments.home.HomeRecyclerAdapter;
+import com.example.giveandtake.model.Post;
+import com.example.giveandtake.model.PostImageActivity;
+import com.example.giveandtake.model.PostModel;
+import com.example.giveandtake.model.User;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Profile extends Fragment {
+
+    private TextView volunteerStatusTv;
+    private TextView ageTv, workTv, addressTv, nameTv;
+    private CircleImageView profileImageView;
+    private FirebaseUser fireUser;
+    String profileUserId;
+    FragmentProfileBinding binding;
+    HomeRecyclerAdapter adapter;
+    HomeListFragmentViewModel viewModel;
+
+    public Profile() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+
+        init(view);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        fireUser = auth.getCurrentUser();
+        profileUserId = fireUser.getUid();
+        DocumentReference docRef = FirebaseFirestore.getInstance().collection("User").document(profileUserId);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                nameTv.setText(user.getName());
+                volunteerStatusTv.setText("Volunteering: "+ user.getVolunteerStatus());
+                //profileImage.setImageURI(Uri.parse(documentSnapshot.getString("profileImageURL")));
+                if(documentSnapshot.getString("workAt") == null)
+                    workTv.setText("Work At: ");
+                else
+                     workTv.setText("Work At: "+user.getWorkAt());
+                addressTv.setText("Address: "+user.getAddress());
+                ageTv.setText("Age: " +user.getAge());
+                if (user.getProfileImage() != "" && user.getProfileImage().length() > 5) {
+                    Picasso.get().load(user.getProfileImage()).placeholder(R.drawable.ic_person).into(profileImageView);
+                }else{
+                    profileImageView.setImageResource(R.drawable.ic_person);
+                }
+            }
+        });
+        View editProfile = view.findViewById(R.id.editProfileBtn);
+        NavDirections action = ProfileDirections.actionProfileFragmentToEditProfileFragment();
+        editProfile.setOnClickListener(Navigation.createNavigateOnClickListener(action));
+
+        binding.profileRecycle.setHasFixedSize(true);
+        binding.profileRecycle.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new HomeRecyclerAdapter(getLayoutInflater(),viewModel.getData().getValue());
+        binding.profileRecycle.setAdapter(adapter);
+
+        List<Post> userPosts = new LinkedList<>();
+        viewModel.getData().observe(getViewLifecycleOwner(),list->{
+            if(list.size() != 0){
+                for(int i=list.size()-1;i>=0;i--)
+                {
+                    if(list.get(i).userId.equals(profileUserId))
+                        userPosts.add(list.get(i));
+                }
+                adapter.setData(userPosts);
+            }
+        });
+
+        return view;
+    }
+
+    public void init(View view){
+        nameTv = view.findViewById(R.id.profile_nameTv);
+        volunteerStatusTv = view.findViewById(R.id.profile_volunteerTv);
+        profileImageView = view.findViewById(R.id.profile_profileImage);
+        workTv = view.findViewById(R.id.workTv);
+        addressTv = view.findViewById(R.id.addressTv);
+        ageTv = view.findViewById(R.id.birthdayTv);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        viewModel = new ViewModelProvider(this).get(HomeListFragmentViewModel.class);
+    }
+
+    void reloadData(){
+        PostModel.instance().refreshAllPosts();
+    }
+}
---
+++ app/src/main/java/com/example/giveandtake/fragments/profile/ProfileEdit.java
@@ -0,0 +1,192 @@
+package com.example.giveandtake.fragments.profile;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import com.example.giveandtake.R;
+import com.example.giveandtake.databinding.FragmentProfileEditBinding;
+import com.example.giveandtake.model.User;
+import com.example.giveandtake.model.UserModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProfileEdit extends Fragment {
+
+    EditText ageET, addressET, workPlaceET, volunteerET, nameET;
+    Button updateBtn;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private FirebaseAuth auth;
+    ProgressBar progressBar;
+    FragmentProfileEditBinding binding;
+    ActivityResultLauncher<Void> cameraLauncher;
+    ActivityResultLauncher<String> galleryLauncher;
+    Boolean isProfileSelected = false;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        cameraLauncher = registerForActivityResult(new ActivityResultContracts.TakePicturePreview(), new ActivityResultCallback<Bitmap>() {
+            @Override
+            public void onActivityResult(Bitmap result) {
+                if (result != null) {
+                    binding.editProfileImage.setImageBitmap(result);
+                    isProfileSelected = true;
+                }
+            }
+        });
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                if (result != null){
+                    binding.editProfileImage.setImageURI(result);
+                    isProfileSelected = true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentProfileEditBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        binding.profileSaveBtn.setOnClickListener(view1->{
+            String age = ageET.getText().toString();
+            String work = workPlaceET.getText().toString();
+            String address = addressET.getText().toString();
+            String volunteer = volunteerET.getText().toString();
+            String name = nameET.getText().toString();
+            progressBar.setVisibility(View.VISIBLE);
+
+            if (isProfileSelected){
+                binding.editProfileImage.setDrawingCacheEnabled(true);
+                binding.editProfileImage.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) binding.editProfileImage.getDrawable()).getBitmap();
+                UserModel.instance().uploadImage(user.getUid(), bitmap, url->{
+                    DocumentReference docRef = FirebaseFirestore.getInstance().collection("User").document(user.getUid());
+                    docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            User userEdit = documentSnapshot.toObject(User.class);
+                            if (url != null){
+                                userEdit.setProfileImage(url);
+                                updateAccount(age, work, address, volunteer,name,url);
+                            }
+                        }
+                    });
+                });
+            }else
+                updateAccount(age, work, address, volunteer,name,"");
+            Navigation.findNavController(view1).popBackStack(R.id.profileFragment,false);
+        });
+        binding.cancellBtn.setOnClickListener(view1 -> Navigation.findNavController(view1).popBackStack(R.id.profileFragment,false));
+
+        binding.cameraButton.setOnClickListener(view1->{
+            cameraLauncher.launch(null);
+        });
+
+        binding.galleryButton.setOnClickListener(view1->{
+            galleryLauncher.launch("image/*");
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        NavController navController = NavHostFragment.findNavController(this);
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+    }
+
+    private void init(View view) {
+        ageET = view.findViewById(R.id.birthdayTv_edit);
+        addressET = view.findViewById(R.id.addressTv_edit);
+        workPlaceET = view.findViewById(R.id.workTv_edit);
+        volunteerET = view.findViewById(R.id.profile_volunteerTv_edit);
+        updateBtn = view.findViewById(R.id.profile_saveBtn);
+        auth = FirebaseAuth.getInstance();
+        nameET = view.findViewById(R.id.edit_nameTv);
+        progressBar = view.findViewById(R.id.update_progressBar);
+    }
+
+    private void updateAccount( String age, String work, String address, String volunteer, String name, String profileImage)
+    {
+        auth.updateCurrentUser(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(getContext(), "Profile Updated successfully", Toast.LENGTH_SHORT).show();
+                }
+                uploadUpdatedUser(user,age, work, address, volunteer, name, profileImage);
+
+            }
+        });
+    }
+
+    private void uploadUpdatedUser(FirebaseUser user,String age,String work, String address, String volunteer , String name, String profileImage) {
+        Map<String, Object> map = new HashMap<>();
+        if(!age.equals(""))
+            map.put("age", age);
+        if(!work.equals(""))
+            map.put("workAt", work);
+        if(!address.equals(""))
+            map.put("address", address);
+        if(!volunteer.equals(""))
+            map.put("volunteerStatus", volunteer);
+        if(!name.equals(""))
+            map.put("name", name);
+        if(!profileImage.equals(""))
+            map.put("profileImage", profileImage);
+
+
+        FirebaseFirestore.getInstance().collection("User").document(user.getUid()).update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    String exception = task.getException().getMessage();
+                    Toast.makeText(getContext(), "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
